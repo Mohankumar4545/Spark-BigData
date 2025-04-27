20b8eb471a1ff8bf5d7fd701ff54d7acfab75207
Mohankumar R <mohankumar0805r@gmail.com>
2025-04-27 22:43:23 +0530

    Removed AWS access keys from code

--- utils/SparkSession.py
+++ utils/SparkSession.py
@@ -4,7 +4,7 @@ def SessionCreator(Appname):
   return SparkSession.builder\
     .appName(Appname) \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")\
-    .config("spark.hadoop.fs.s3a.access.key", "AKIA2ZIOMXVMFCUBPTOO") \
-    .config("spark.hadoop.fs.s3a.secret.key", "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") \
+    .config("spark.hadoop.fs.s3a.access.key", "AccessKey") \
+    .config("spark.hadoop.fs.s3a.secret.key", "Secret key") \
     .config("spark.hadoop.fs.s3a.endpoint", "s3.ap-south-1.amazonaws.com") \
     .getOrCreate()
