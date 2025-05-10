5308e4ef01af03ae9ac32a8495651b33ee8d705f
Mohankumar R <mohankumar0805r@gmail.com>
2025-05-10 13:17:05 +0530

    feat: updated SparkSession to read from S3 and configured session utils

--- utils/SparkSession.py
+++ utils/SparkSession.py
@@ -1,10 +1,18 @@
 from pyspark.sql import SparkSession
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  
 
 def SessionCreator(Appname):
+  awsAccessKey=os.getenv("awsAccessKey")
+  awsSecretkey=os.getenv("SecretAccessKey")
+  s3EndPoint=os.getenv("S3_ENDPOINT")
+
   return SparkSession.builder\
     .appName(Appname) \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")\
-    .config("spark.hadoop.fs.s3a.access.key", "AccessKey") \
-    .config("spark.hadoop.fs.s3a.secret.key", "Secret key") \
-    .config("spark.hadoop.fs.s3a.endpoint", "s3.ap-south-1.amazonaws.com") \
+    .config("spark.hadoop.fs.s3a.access.key", awsAccessKey) \
+    .config("spark.hadoop.fs.s3a.secret.key", awsSecretkey) \
+    .config("spark.hadoop.fs.s3a.endpoint", s3EndPoint) \
     .getOrCreate()
